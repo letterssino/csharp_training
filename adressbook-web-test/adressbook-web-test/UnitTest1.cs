bc4bf5f906f8cc4fc5f63313540b4ca77e377259
VirtoriaAnikina <lor.vick@yandex.ru>
2023-12-07 14:34:08 +0300

    Проект создан

---
+++ adressbook-web-test/adressbook-web-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace adressbook_web_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
